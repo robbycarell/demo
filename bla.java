2638650eac88568909730abc52dbdd0d092858e4
robbycarell <51292442+robbycarell@users.noreply.github.com>
2019-06-03 14:31:01 +0200

    Update bla.java

--- bla.java
+++ bla.java
@@ -2,7 +2,14 @@
 //dit heb ik toegevoegd
 //en dit ook
 
+    class Simple{  
+        public static void main(String args[]){  
+         System.out.println("Hello World!");  
+        }  
+    }  
+
 public class clone {
 int x =10;
-
 }
+
+
